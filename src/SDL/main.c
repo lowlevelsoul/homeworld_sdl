aff4c279f913b4b88a70bf47a97047bcc290eb74
James Steele <jad.steele@gmail.com>
2021-07-01 16:07:52 +0200

    Set to windowed for debug builds

--- src/SDL/main.c
+++ src/SDL/main.c
@@ -176,8 +176,8 @@ bool GLOBAL_NO_TEXTURES = FALSE;
 
 // turn fullscreen off when debugging so that if the debugger kicks in
 // after a crash you don't find yourself locked out and have to reboot...
-#if defined(_MACOSX) && defined(HW_BUILD_FOR_DEBUGGING) 
-bool fullScreen = TRUE;
+#if defined(HW_BUILD_FOR_DEBUGGING) 
+bool fullScreen = FALSE;
 #else
 bool fullScreen = TRUE;
 #endif
